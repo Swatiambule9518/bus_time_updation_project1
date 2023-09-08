9d37e80a4dd920846e2183bfa8318342e6c2b91e
Swatiambule9518 <118324379+Swatiambule9518@users.noreply.github.com>
2023-09-08 11:41:28 +0530

    Create info.php

---
+++ info.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+      background-image:url("buss.jpg");
+    </style>
+</head>
+<body>
+<h2><center> Bus Information</center></h2>
+
+<nav class="navbar bg-light">
+  <form class="container-fluid">
+    <div class="input-group">
+      <span class="input-group-text" id="basic-addon1"> </span>
+      <input type="text" class="form-control" placeholder="Search " aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+  </form>
+</nav>
+<br>
+<br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">bus no</th>
+      <th scope="col">From</th>
+      <th scope="col">To</th>
+      <th scope="col">Time </th>
+      <th scope="col"> status update</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">3456</th>
+      <td>Pandharpur</td>
+      <td>Solapur</td>
+      <td>12:00 pm</td>
+      <td>on time</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>fyewfghr</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+      <td>fdyfyg3</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</body>
+</html>
